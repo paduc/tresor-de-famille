66930f727e09569cc98872502b4ae8d72153e248
Pierre A. Duchateau <pad@dabla.com>
2022-07-25 14:28:58 +0000

    WIP gedcom upload page

---
+++ src/pages/importGedcom/ImportGedcomPage.stories.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { SessionContext } from '../_components'
+
+import { ImportGedcomPage } from './ImportGedcomPage'
+
+export default { title: "Page d'import Gedcom", component: ImportGedcomPage }
+
+export const Basique = () => (
+  <SessionContext.Provider value={{ isLoggedIn: true, userName: 'toto' }}>
+    <ImportGedcomPage />
+  </SessionContext.Provider>
+)
