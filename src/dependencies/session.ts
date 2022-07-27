d99e0606c49f5afdf37540a592405529c6492e0a
ClementCHA <73785703+ClementCHA@users.noreply.github.com>
2022-07-27 12:06:36 +0200

    Keep postgres encapsulated inside session.ts

--- src/dependencies/session.ts
+++ src/dependencies/session.ts
@@ -1,7 +1,12 @@
 import session from 'express-session';
 import connectPgSimple from 'connect-pg-simple';
 
- 
-export const sessionStore = new session.MemoryStore();
+import { postgres } from '../dependencies/postgres';
 
-export const pgSession = connectPgSimple(session)
+const pgSession = connectPgSimple(session)
+
+export const sessionStore = new pgSession({
+  pool: postgres,
+  createTableIfMissing: true,
+  tableName: 'user_sessions'
+});
