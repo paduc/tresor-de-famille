82c7fa979dc427c666947b500611a2921ac6157f
ClementCHA <73785703+ClementCHA@users.noreply.github.com>
2022-08-02 09:07:55 +0200

    Toast closing either with button, either with setTimeOut. Rest : Doing some style with Tailwind/css transition-animation

--- src/pages/_components/SuccessNotification.tsx
+++ src/pages/_components/SuccessNotification.tsx
@@ -9,6 +9,9 @@ export const SuccessNotification = () => {
     }, 3000)
   }, [])
 
+  const onToastClick = () => {
+    setShowElement(false)
+  }
   return (
     <>
       {showElement && (
@@ -32,6 +35,7 @@ export const SuccessNotification = () => {
           </div>
           <div className='ml-3 text-sm font-normal'>Item moved successfully.</div>
           <button
+            onClick={onToastClick}
             type='button'
             className='ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700'
             data-dismiss-target='#toast-success'
