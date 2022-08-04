04c9cf12aff02c4bf0778e197503c413e972ba0d
ClementCHA <73785703+ClementCHA@users.noreply.github.com>
2022-08-04 15:07:45 +0200

    Clean from comments, add importBy in GedcomImported payload

--- src/pages/importGedcomSuccess/ImportGedcomSuccess.route.ts
+++ src/pages/importGedcomSuccess/ImportGedcomSuccess.route.ts
@@ -7,19 +7,8 @@ import { getGedcom } from './getGedcom.query'
 pageRouter.route('/importGedcomSuccess.html').get(requireAuth(), async (request, response) => {
   console.log(`GET on /importGedcomSucess.html`)
 
-  // @ts-ignore
-  return returnImportGedcomSuccesPage(request, response)
-})
-
-/* pageRouter.post('/importGedcomSuccess.html').post(async (request, response) => {
-  return 'test'
-})
- */
-export const returnImportGedcomSuccesPage = async (request: Request, response: Response) => {
   const gedcom = await getGedcom()
-
-  /* if (!gedcom) return response.status(404).send() */
 
   // @ts-ignore
   responseAsHtml(request, response, ImportGedcomSuccessPage({ gedcom }))
-}
+})
