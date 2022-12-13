baa4268a37128d2521331bc5175dce8dd3d4e446
Pierre Duchateau <pad@dabla.com>
2022-12-13 17:34:38 +0100

    Add migration success/error logs

--- src/migrate.ts
+++ src/migrate.ts
@@ -5,30 +5,39 @@ import { tables } from './tables/tables'
 import { seed } from './seed'
 
 async function migrate() {
-  // Create the event store
-  await initEventStore()
+  console.log('Starting migration...')
 
-  if (SEED && process.env.NODE_ENV !== 'test') {
-    // Seed the history
-    await seed()
-  }
+  try {
+    // Create the event store
+    await initEventStore()
 
-  // Step 1: prepare the projections (ex: create the table)
-  const projectionsToRebuild: ProjectionTable[] = []
-  for (const projection of tables) {
-    if (await projection.requiresRebuild()) {
-      await projection.reset()
-      projectionsToRebuild.push(projection)
+    if (SEED && process.env.NODE_ENV !== 'test') {
+      // Seed the history
+      await seed()
     }
+
+    // Step 1: prepare the projections (ex: create the table)
+    const projectionsToRebuild: ProjectionTable[] = []
+    for (const projection of tables) {
+      if (await projection.requiresRebuild()) {
+        await projection.reset()
+        projectionsToRebuild.push(projection)
+      }
+    }
+
+    // Step 2: Build the projections from history
+    const events = await getHistory()
+    for (const event of events) {
+      for (const projection of projectionsToRebuild) {
+        await projection.handleEvent(event)
+      }
+    }
+  } catch (error) {
+    console.log('Migration failed', error)
+    process.exit(1)
   }
 
-  // Step 2: Build the projections from history
-  const events = await getHistory()
-  for (const event of events) {
-    for (const projection of projectionsToRebuild) {
-      await projection.handleEvent(event)
-    }
-  }
+  console.log('Migration succes !')
 
   process.exit(0)
 }
