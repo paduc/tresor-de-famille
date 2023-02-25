e2643ba75fcb866b20543a4467acde4c5a2f996e
Pierre Duchateau <pad@dabla.com>
2023-02-25 14:27:05 +0100

    Add message textarea

--- src/pages/chat/ChatPage.tsx
+++ src/pages/chat/ChatPage.tsx
@@ -212,6 +212,17 @@ function AddPhotoIcon(props: any) {
     </svg>
   )
 }
+function SendIcon(props: any) {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' {...props}>
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5'
+      />
+    </svg>
+  )
+}
 type UploadPhotoItemProps = {}
 const UploadPhotoItem = ({}: UploadPhotoItemProps) => {
   return (
@@ -224,10 +235,52 @@ const UploadPhotoItem = ({}: UploadPhotoItemProps) => {
         </div>
       </div>
       <div className='min-w-0 flex-1'>
+        <form action='#' className='relative'>
+          <div className='overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500'>
+            <label htmlFor='comment' className='sr-only'>
+              Ajouter un message
+            </label>
+            <textarea
+              rows={3}
+              name='comment'
+              id='comment'
+              className='block w-full resize-none border-0 py-3 focus:ring-0 sm:text-sm'
+              placeholder='Ajouter un message...'
+              defaultValue={''}
+            />
+
+            {/* Spacer element to match the height of the toolbar */}
+            <div className='py-2' aria-hidden='true'>
+              {/* Matches height of button in toolbar (1px border + 36px content height) */}
+              <div className='py-px'>
+                <div className='h-9' />
+              </div>
+            </div>
+          </div>
+
+          <div className='absolute inset-x-0 bottom-0 flex justify-between py-2 pl-3 pr-2'>
+            <div className='flex-shrink-0'>
+              <button
+                type='submit'
+                className='inline-flex items-center mt-3 px-3 py-1.5 border border-transparent sm:sm:text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+                <SendIcon className='-ml-0.5 mr-2 h-4 w-4' aria-hidden='true' />
+                Envoyer
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className='relative mt-2'>
+          <div className='absolute inset-0 flex items-center' aria-hidden='true'>
+            <div className='w-full border-t border-gray-200' />
+          </div>
+          <div className='relative flex justify-center'>
+            <span className='bg-white px-2 text-sm text-gray-500'>ou</span>
+          </div>
+        </div>
         <button
           type='button'
-          className='inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
-          <AddPhotoIcon className='-ml-0.5 mr-2 h-4 w-4' aria-hidden='true' />
+          className='inline-flex items-center mt-3 px-3 py-1.5 border border-transparent sm:text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+          <PhotoIcon className='-ml-0.5 mr-2 h-4 w-4' aria-hidden='true' />
           Ajouter une photo
         </button>
       </div>
