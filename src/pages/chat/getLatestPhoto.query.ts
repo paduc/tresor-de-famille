53c4246aebe24ff729d951619a945dcf5d575eec
Pierre Duchateau <pad@dabla.com>
2023-03-08 12:03:35 +0100

    implement getLatestPhoto to get the latest photo in the chat

---
+++ src/pages/chat/getLatestPhoto.query.ts
@@ -0,0 +1,77 @@
+import Rekognition from 'aws-sdk/clients/rekognition'
+import { postgres } from '../../dependencies/postgres'
+import { FacesRecognizedInChatPhoto } from './FacesRecognizedInChatPhoto'
+import { UserUploadedPhotoToChat } from './UserUploadedPhotoToChat'
+
+type PhotoFace = {
+  details: {
+    age?: { low: number; high: number }
+    gender?: 'M' | 'F'
+  }
+} & (
+  | {
+      personId: null
+      faceCode: string
+    }
+  | { personId: string; faceCode: null }
+)
+
+export const getLatestPhotoFaces = async (chatId: string): Promise<PhotoFace[] | null> => {
+  const { rows: latestPhotos } = await postgres.query<UserUploadedPhotoToChat>(
+    "SELECT * FROM events WHERE type='UserUploadedPhotoToChat' AND payload->>'chatId'=$1 ORDER BY 'occurred_at` DESC LIMIT 1",
+    [chatId]
+  )
+
+  if (!latestPhotos.length) {
+    return null
+  }
+
+  const latestPhoto = latestPhotos[0].payload
+  const { rows: latestPhotoFacesList } = await postgres.query<FacesRecognizedInChatPhoto>(
+    "SELECT * FROM events WHERE type='FacesRecognizedInChatPhoto' AND payload->>'chatId'=$1 AND payload->>'photoId'=$2 ORDER BY 'occurred_at` DESC LIMIT 1",
+    [chatId, latestPhoto.photoId]
+  )
+
+  if (!latestPhotoFacesList.length) {
+    return null
+  }
+
+  const latestPhotoFaces = latestPhotoFacesList[0].payload.faces
+
+  return latestPhotoFaces.map((face) =>
+    face.personId
+      ? {
+          personId: face.personId,
+          faceCode: null,
+          details: {
+            gender: getGender(face.details),
+            age: getAge(face.details),
+          },
+        }
+      : {
+          personId: null,
+          faceCode: 'A',
+          details: {
+            gender: getGender(face.details),
+            age: getAge(face.details),
+          },
+        }
+  )
+}
+
+function getGender(faceDetail: Rekognition.FaceDetail | undefined): PhotoFace['details']['gender'] {
+  if (!faceDetail || !faceDetail.Gender || !faceDetail.Gender.Confidence) return undefined
+
+  if (faceDetail.Gender.Value && faceDetail.Gender.Confidence > 90) {
+    return faceDetail.Gender.Value === 'Male' ? 'M' : faceDetail.Gender.Value === 'Female' ? 'F' : undefined
+  }
+}
+
+function getAge(faceDetail: Rekognition.FaceDetail | undefined): PhotoFace['details']['age'] {
+  if (!faceDetail || !faceDetail.AgeRange) return undefined
+
+  return {
+    low: faceDetail.AgeRange.Low!,
+    high: faceDetail.AgeRange.High!,
+  }
+}
