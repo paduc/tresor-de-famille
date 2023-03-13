520298928cc340e63d5c2d6c05e35b266c6eb741
Pierre Duchateau <pad@dabla.com>
2023-03-13 15:04:59 +0100

    Refactor

--- src/pages/chat/getChatHistory/retrievePhotosForChat.ts
+++ src/pages/chat/getChatHistory/retrievePhotosForChat.ts
@@ -1,8 +1,10 @@
 import { postgres } from '../../../dependencies/postgres'
+import { normalizeBBOX } from '../../../dependencies/rekognition'
 import { getPhotoUrlFromId, getProfilePicUrlForUser } from '../../../dependencies/uploadPhoto'
 import { ChatEvent } from '../ChatPage/ChatPage'
+import { getPersonById } from '../getPersonById.query'
+import { FacesRecognizedInChatPhoto } from '../recognizeFacesInChatPhoto/FacesRecognizedInChatPhoto'
 import { UserUploadedPhotoToChat } from '../uploadPhotoToChat/UserUploadedPhotoToChat'
-import { augmentChatPhotosWithFacesDetected } from './augmentChatPhotosWithFacesDetected'
 
 export async function retrievePhotosForChat(chatId: string) {
   const { rows: photoRowsRes } = await postgres.query<UserUploadedPhotoToChat>(
@@ -23,4 +25,40 @@ export async function retrievePhotosForChat(chatId: string) {
 
   await augmentChatPhotosWithFacesDetected(chatId, photoRows)
   return photoRows
+}
+
+async function augmentChatPhotosWithFacesDetected(
+  chatId: string,
+  photoRows: Awaited<ReturnType<typeof retrievePhotosForChat>>
+) {
+  const { rows: faceDetectedRowsRes } = await postgres.query<FacesRecognizedInChatPhoto>(
+    "SELECT * FROM events WHERE type='FacesRecognizedInChatPhoto' AND payload->>'chatId'=$1",
+    [chatId]
+  )
+
+  const facesDetectedRows = faceDetectedRowsRes.map((row) => row.payload)
+  for (const facesDetectedRow of facesDetectedRows) {
+    const photoRow = photoRows.find((row) => row.photo.id === facesDetectedRow.photoId)
+
+    if (!photoRow) continue
+
+    for (const awsFace of facesDetectedRow.faces) {
+      let personName = null
+      if (awsFace.personId) {
+        const person = await getPersonById(awsFace.personId)
+        if (person) {
+          personName = person.name
+        }
+      }
+
+      photoRow.photo.faces = [
+        ...(photoRow.photo.faces || []),
+        {
+          person: personName ? { name: personName } : null,
+          faceId: awsFace.AWSFaceId,
+          position: normalizeBBOX(awsFace.position),
+        },
+      ]
+    }
+  }
 }
