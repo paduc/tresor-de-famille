e86a5f8fd0ee93d58db49bcd384a9101962506d6
Pierre Duchateau <pad@dabla.com>
2023-04-25 11:40:34 +0200

    fix story

--- src/pages/photo/PhotoPage/PhotoPage.stories.tsx
+++ src/pages/photo/PhotoPage/PhotoPage.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { SessionContext } from '../../_components'
 
+import { getUuid } from '../../../libs/getUuid'
 import { ChatEvent, PhotoPage } from './PhotoPage'
 
 export default { title: 'Ajouter une Photo', component: PhotoPage, parameters: { layout: 'fullscreen' } }
@@ -15,12 +16,12 @@ const starterHistory: ChatEvent[] = [
     profilePicUrl:
       'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
     photo: {
-      id: 'photo123',
+      id: getUuid(),
       url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=500&h=500&q=80',
       faces: [
         {
           person: null,
-          faceId: 'face123',
+          faceId: getUuid(),
           position: {
             width: 0.3004770278930664,
             height: 0.39314860105514526,
@@ -48,7 +49,7 @@ const starterHistory: ChatEvent[] = [
         person: {
           name: 'Toto',
         },
-        faceId: 'face123',
+        faceId: getUuid(),
         photo: {
           url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=500&h=500&q=80',
         },
@@ -73,7 +74,7 @@ export const ApresUploadPhotoLarge = () => (
   <SessionContext.Provider value={{ isLoggedIn: true, userName: 'toto', isAdmin: false }}>
     <PhotoPage
       photo={{
-        id: '',
+        id: getUuid(),
         url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=4032&h=3024&q=80',
       }}
     />
@@ -83,7 +84,7 @@ export const ApresUploadPhotoHaute = () => (
   <SessionContext.Provider value={{ isLoggedIn: true, userName: 'toto', isAdmin: false }}>
     <PhotoPage
       photo={{
-        id: '',
+        id: getUuid(),
         url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=3024&h=4032&q=80',
       }}
     />
@@ -94,12 +95,12 @@ export const PhotoAvecVisagesInconnu = () => (
   <SessionContext.Provider value={{ isLoggedIn: true, userName: 'toto', isAdmin: false }}>
     <PhotoPage
       photo={{
-        id: '',
+        id: getUuid(),
         url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=4032&h=3024&q=80',
         faces: [
           {
             person: null,
-            faceId: 'face123',
+            faceId: getUuid(),
             position: {
               width: 0.3004770278930664,
               height: 0.39314860105514526,
@@ -117,7 +118,7 @@ export const PhotoAvecVisagesConnus = () => (
   <SessionContext.Provider value={{ isLoggedIn: true, userName: 'toto', isAdmin: false }}>
     <PhotoPage
       photo={{
-        id: '',
+        id: getUuid(),
         url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=4032&h=3024&q=80',
         faces: [
           {
@@ -125,7 +126,7 @@ export const PhotoAvecVisagesConnus = () => (
               name: 'Toto',
               annotatedBy: 'ai',
             },
-            faceId: 'face123',
+            faceId: getUuid(),
             position: {
               width: 0.3004770278930664,
               height: 0.39314860105514526,
@@ -138,7 +139,7 @@ export const PhotoAvecVisagesConnus = () => (
               name: 'Fantome',
               annotatedBy: 'face-recognition',
             },
-            faceId: 'face124',
+            faceId: getUuid(),
             position: {
               width: 0.3004770278930664,
               height: 0.39314860105514526,
@@ -156,7 +157,7 @@ export const PhotoAvecLegende = () => (
   <SessionContext.Provider value={{ isLoggedIn: true, userName: 'toto', isAdmin: false }}>
     <PhotoPage
       photo={{
-        id: '',
+        id: getUuid(),
         url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=4032&h=3024&q=80',
         faces: [
           {
@@ -164,7 +165,7 @@ export const PhotoAvecLegende = () => (
               name: 'Toto',
               annotatedBy: 'ai',
             },
-            faceId: 'face123',
+            faceId: getUuid(),
             position: {
               width: 0.3004770278930664,
               height: 0.39314860105514526,
