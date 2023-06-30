60ee9a39d940c8d4b0e1c9bfa61626dbc8d18d20
Pierre Duchateau <pad@dabla.com>
2023-06-30 19:28:10 +0200

    WIP step 3, getPreviousMessages (get faces and their stages)

--- src/pages/bienvenue/getPreviousMessages.ts
+++ src/pages/bienvenue/getPreviousMessages.ts
@@ -56,38 +56,57 @@ export async function getPreviousMessages(userId: UUID): Promise<BienvenuePagePr
 
   if (props.steps.at(-1)?.stage === 'done') {
     // Onboarding is a special chat thread with chatId = userId
-    const { rows: userUploadedPhoto } = await postgres.query<UserUploadedPhotoToChat>(
-      "SELECT * FROM history WHERE type='UserUploadedPhotoToChat' AND payload->>'uploadedBy'=$1 AND payload->>'chatId'=$2 ORDER BY \"occurredAt\" DESC LIMIT 1",
-      [userId, userId]
+
+    // 1) Has user confirmed face ?
+    // 2) Has user uploaded photo ?
+    // 3) Do we have faces ?
+
+    // Has the user confirmed their face ?
+    const { rows: userConfirmedFace } = await postgres.query<UserConfirmedHisFaceDuringOnboarding>(
+      "SELECT * FROM history WHERE type='UserConfirmedHisFaceDuringOnboarding' AND payload->>'userId'=$1 ORDER BY \"occurredAt\" DESC LIMIT 1",
+      [userId]
     )
 
-    if (userUploadedPhoto.length) {
-      const { photoId } = userUploadedPhoto[0].payload
+    if (userConfirmedFace.length) {
+      // User confirmed his face, we are DONE
 
-      // Do we have faces ?
+      const { photoId } = userConfirmedFace[0].payload
+
       const { rows: facesDetected } = await postgres.query<AWSDetectedFacesInPhoto>(
         "SELECT * FROM history WHERE type='AWSDetectedFacesInPhoto' AND payload->>'photoId'=$1 ORDER BY \"occurredAt\" DESC LIMIT 1",
         [photoId]
       )
-
       const faces = facesDetected[0]?.payload.faces.map(({ faceId }) => ({ faceId }))
 
-      // Has the user confirmed their face ?
-      const { rows: userConfirmedFace } = await postgres.query<UserConfirmedHisFaceDuringOnboarding>(
-        "SELECT * FROM history WHERE type='UserConfirmedHisFaceDuringOnboarding' AND payload->>'userId'=$1 ORDER BY \"occurredAt\" DESC LIMIT 1",
-        [userId]
+      props.steps.push({
+        goal: 'upload-first-photo',
+        stage: 'face-confirmed',
+        photoId,
+        photoUrl: getPhotoUrlFromId(photoId),
+        faces,
+        confirmedFaceId: userConfirmedFace[0].payload.faceId,
+      })
+    } else {
+      // Has the user uploaded a photo ?
+
+      const { rows: userUploadedPhoto } = await postgres.query<UserUploadedPhotoToChat>(
+        "SELECT * FROM history WHERE type='UserUploadedPhotoToChat' AND payload->>'uploadedBy'=$1 AND payload->>'chatId'=$2 ORDER BY \"occurredAt\" DESC LIMIT 1",
+        [userId, userId]
       )
 
-      if (userConfirmedFace.length) {
-        props.steps.push({
-          goal: 'upload-first-photo',
-          stage: 'face-confirmed',
-          photoId,
-          photoUrl: getPhotoUrlFromId(photoId),
-          faces,
-          confirmedFaceId: userConfirmedFace[0].payload.faceId,
-        })
-      } else {
+      if (userUploadedPhoto.length) {
+        // At least one photo has been uploaded
+
+        const { photoId } = userUploadedPhoto[0].payload
+
+        // Do we have faces ?
+        const { rows: facesDetected } = await postgres.query<AWSDetectedFacesInPhoto>(
+          "SELECT * FROM history WHERE type='AWSDetectedFacesInPhoto' AND payload->>'photoId'=$1 ORDER BY \"occurredAt\" DESC LIMIT 1",
+          [photoId]
+        )
+
+        const faces = facesDetected[0]?.payload.faces.map(({ faceId }) => ({ faceId }))
+
         props.steps.push({
           goal: 'upload-first-photo',
           stage: 'photo-uploaded',
@@ -95,22 +114,51 @@ export async function getPreviousMessages(userId: UUID): Promise<BienvenuePagePr
           photoUrl: getPhotoUrlFromId(photoId),
           faces,
         })
+      } else {
+        // No photo uploaded
+
+        props.steps.push({
+          goal: 'upload-first-photo',
+          stage: 'waiting-upload',
+        })
       }
-    } else {
-      props.steps.push({
-        goal: 'upload-first-photo',
-        stage: 'waiting-upload',
-      })
     }
   }
 
   // Step 3 : User Uploads family photo
 
   if (props.steps.at(-1)?.stage === 'face-confirmed') {
-    props.steps.push({
-      goal: 'upload-family-photo',
-      stage: 'awaiting-upload',
-    })
+    // TODO: Guess if user has uploaded a photo for his family (is last photo of the thread the solo picture ?)
+
+    const { rows: userConfirmedFace } = await postgres.query<UserConfirmedHisFaceDuringOnboarding>(
+      "SELECT * FROM history WHERE type='UserConfirmedHisFaceDuringOnboarding' AND payload->>'userId'=$1 ORDER BY \"occurredAt\" DESC LIMIT 1",
+      [userId]
+    )
+
+    const { photoId: photoIdFromConfirmedFace } = userConfirmedFace[0].payload
+
+    const { rows: latestUserUploadedPhoto } = await postgres.query<UserUploadedPhotoToChat>(
+      "SELECT * FROM history WHERE type='UserUploadedPhotoToChat' AND payload->>'uploadedBy'=$1 AND payload->>'chatId'=$2 ORDER BY \"occurredAt\" DESC LIMIT 1",
+      [userId, userId]
+    )
+
+    const { photoId: latestPhotoId } = latestUserUploadedPhoto[0].payload
+
+    if (photoIdFromConfirmedFace === latestPhotoId) {
+      // No the user has not uploaded another photo for his family
+      props.steps.push({
+        goal: 'upload-family-photo',
+        stage: 'awaiting-upload',
+      })
+    } else {
+      // Has the user annotated a face ?
+      const { rows: facesDetected } = await postgres.query<AWSDetectedFacesInPhoto>(
+        "SELECT * FROM history WHERE type='AWSDetectedFacesInPhoto' AND payload->>'photoId'=$1 ORDER BY \"occurredAt\" DESC LIMIT 1",
+        [latestPhotoId]
+      )
+
+      // Get facesDetected
+    }
   }
 
   return props
