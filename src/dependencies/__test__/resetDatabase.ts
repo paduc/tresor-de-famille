c335b81e27cdb7b97d729d781467083f0788eaa4
Pierre Duchateau <pad@dabla.com>
2023-07-21 18:03:33 +0200

    Add integration test capabilities

--- src/dependencies/__test__/resetDatabase.ts
+++ src/dependencies/__test__/resetDatabase.ts
@@ -1,12 +1,15 @@
+import { createHistoryTable } from '../addToHistory'
 import { postgres } from '../database'
 
 export const resetDatabase = async () => {
   const client = await postgres.connect()
 
+  await createHistoryTable()
+
   try {
     await client.query('BEGIN')
 
-    await client.query(`TRUNCATE events`)
+    await client.query(`TRUNCATE history`)
 
     await client.query('COMMIT')
   } catch (error) {
