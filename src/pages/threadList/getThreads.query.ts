b40c8f2834fc65f1891de531c6230bf13e13deaf
Pierre Duchateau <pad@dabla.com>
2023-07-24 17:43:57 +0200

    fix getThreads.query.ts to include onboarding thread

--- src/pages/threadList/getThreads.query.ts
+++ src/pages/threadList/getThreads.query.ts
@@ -1,15 +1,20 @@
 import { postgres } from '../../dependencies/database'
+import { getEventList } from '../../dependencies/getEventList'
 import { UUID } from '../../domain'
+import { OnboardingUserStartedFirstThread } from '../bienvenue/step4-start-thread/OnboardingUserStartedFirstThread'
 import { UserSentMessageToChat } from '../chat/sendMessageToChat/UserSentMessageToChat'
 
 export const getThreads = async (userId: UUID): Promise<{ chatId: UUID; title: string }[]> => {
-  const { rows } = await postgres.query<UserSentMessageToChat>(
-    "SELECT * FROM history WHERE type = 'UserSentMessageToChat' AND payload->>'sentBy'=$1",
-    [userId]
+  const threads = await getEventList<UserSentMessageToChat | OnboardingUserStartedFirstThread>(
+    ['OnboardingUserStartedFirstThread', 'UserSentMessageToChat'],
+    { userId }
   )
 
-  return rows.map((row) => ({
-    chatId: row.payload.chatId,
-    title: row.payload.message,
-  }))
+  return threads.map((row) => {
+    const threadId = row.type === 'OnboardingUserStartedFirstThread' ? row.payload.threadId : row.payload.chatId
+    return {
+      chatId: threadId,
+      title: row.payload.message,
+    }
+  })
 }
