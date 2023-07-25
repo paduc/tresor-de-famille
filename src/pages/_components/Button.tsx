8df195c8d7ee33b4d1ae9f5d7aa5ed7b7c47b859
Pierre Duchateau <pad@dabla.com>
2023-07-25 16:39:07 +0200

    Adjust button styles

--- src/pages/_components/Button.tsx
+++ src/pages/_components/Button.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-export const primaryButtonStyles =
-  'cursor-pointer inline-flex items-center px-3 py-1.5 border border-transparent text-md font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600'
+const sharedButtonStyles =
+  'cursor-pointer inline-flex items-center px-3 py-1.5 border border-transparent text-md font-medium rounded-full shadow-sm'
 
-export const primaryGreenButtonStyles =
-  'cursor-pointer inline-flex items-center px-3 py-1.5 border border-transparent text-md font-medium rounded-full shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500'
+export const primaryButtonStyles = `${sharedButtonStyles} text-white bg-indigo-600 hover:bg-indigo-700`
 
-export const secondaryButtonStyles =
-  'cursor-pointer inline-flex items-center px-3 py-1.5 border border-transparent text-md font-medium rounded-full shadow-sm text-indigo-600 bg-white hover:bg-indigo-600 hover:text-white ring-2 ring-indigo-600'
+export const primaryGreenButtonStyles = `${sharedButtonStyles} text-white bg-green-600 hover:bg-green-700`
 
-export const secondaryGreenButtonStyles =
-  'cursor-pointer inline-flex items-center px-3 py-1.5 border border-transparent text-md font-medium rounded-full shadow-sm text-green-600 bg-white hover:bg-green-600 hover:text-white ring-2 ring-green-600'
+const sharedSecondaryButtonStyles = 'bg-white ring-inset ring-2 hover:text-white'
 
-export const secondaryRedButtonStyles =
-  'cursor-pointer inline-flex items-center px-3 py-1.5 border border-transparent text-md font-medium rounded-full shadow-sm text-red-600 bg-white hover:bg-red-600 hover:text-white ring-2 ring-red-600'
+export const secondaryButtonStyles = `${sharedButtonStyles} ${sharedSecondaryButtonStyles} text-indigo-600 bg-white hover:bg-indigo-600 ring-indigo-600`
+
+export const secondaryGreenButtonStyles = `${sharedButtonStyles} ${sharedSecondaryButtonStyles} text-green-600 bg-white hover:bg-green-600 ring-green-600`
+
+export const secondaryRedButtonStyles = `${sharedButtonStyles} ${sharedSecondaryButtonStyles} text-red-600 bg-white hover:bg-red-600 ring-red-600`
 
 export const buttonIconStyles = '-ml-0.5 mr-2 h-6 w-6'
