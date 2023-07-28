2159e70daf1214fc3f8668da42f9a98717ad1456
Pierre Duchateau <pad@dabla.com>
2023-07-28 13:55:42 +0200

    refactor listPhotos a bit

--- src/pages/listPhotos/getListPhotosProps.ts
+++ src/pages/listPhotos/getListPhotosProps.ts
@@ -4,16 +4,19 @@ import { UUID } from '../../domain'
 import { OnboardingUserUploadedPhotoOfThemself } from '../bienvenue/step1-userTellsAboutThemselves/OnboardingUserUploadedPhotoOfThemself'
 import { OnboardingUserUploadedPhotoOfFamily } from '../bienvenue/step2-userUploadsPhoto/OnboardingUserUploadedPhotoOfFamily'
 import { UserUploadedPhotoToChat } from '../chat/uploadPhotoToChat/UserUploadedPhotoToChat'
+import { ListPhotosProps } from './ListPhotosPage'
 
-export const getPhotos = async (userId: UUID): Promise<{ photoId: UUID; url: string }[]> => {
+export const getListPhotosProps = async (userId: UUID): Promise<ListPhotosProps> => {
   const photos = await getEventList<
     UserUploadedPhotoToChat | OnboardingUserUploadedPhotoOfFamily | OnboardingUserUploadedPhotoOfThemself
   >(['OnboardingUserUploadedPhotoOfFamily', 'OnboardingUserUploadedPhotoOfThemself', 'UserUploadedPhotoToChat'], {
     uploadedBy: userId,
   })
 
-  return photos.map(({ payload: { photoId } }) => ({
-    photoId,
-    url: getPhotoUrlFromId(photoId),
-  }))
+  return {
+    photos: photos.map(({ payload: { photoId } }) => ({
+      photoId,
+      url: getPhotoUrlFromId(photoId),
+    })),
+  }
 }
--- src/pages/listPhotos/listPhotos.route.ts
+++ src/pages/listPhotos/listPhotos.route.ts
@@ -2,11 +2,11 @@ import { responseAsHtml } from '../../libs/ssr/responseAsHtml'
 import { pageRouter } from '../pageRouter'
 import { requireAuth } from '../../dependencies/authn'
 import { ListPhotosPage } from './ListPhotosPage'
-import { getPhotos } from './getPhotos.query'
+import { getListPhotosProps } from './getListPhotosProps'
 import { PhotoListPageUrl } from './PhotoListPageUrl'
 
 pageRouter.route(PhotoListPageUrl).get(requireAuth(), async (request, response) => {
-  const photos = await getPhotos(request.session.user!.id)
+  const props = await getListPhotosProps(request.session.user!.id)
 
-  responseAsHtml(request, response, ListPhotosPage({ photos }))
+  responseAsHtml(request, response, ListPhotosPage(props))
 })
