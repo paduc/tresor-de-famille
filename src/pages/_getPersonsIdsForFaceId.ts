31cad82c034edaab61ceb26c96a7ba3b5998cb48
Pierre Duchateau <pad@dabla.com>
2023-09-15 18:45:45 +0200

    Remove person-face link when there is no link in the photos anymore, when multiple, take person with the most photos

--- src/pages/_getPersonsIdsForFaceId.ts
+++ src/pages/_getPersonsIdsForFaceId.ts
@@ -1,4 +1,5 @@
 import { postgres } from '../dependencies/database'
+import { getEventList } from '../dependencies/getEventList'
 import { UUID } from '../domain'
 import { UserConfirmedHisFace } from '../events/onboarding/UserConfirmedHisFace'
 import { UserNamedPersonInPhoto } from '../events/onboarding/UserNamedPersonInPhoto'
@@ -6,7 +7,7 @@ import { UserRecognizedPersonInPhoto } from '../events/onboarding/UserRecognized
 import { PhotoManuallyAnnotated } from './photo/annotateManually/PhotoManuallyAnnotated'
 import { PhotoAnnotationConfirmed } from './photo/confirmPhotoAnnotation/PhotoAnnotationConfirmed'
 
-export const getPersonIdsForFaceId = async (faceId: UUID): Promise<UUID[]> => {
+export const getPersonIdsForFaceIdOld = async (faceId: UUID): Promise<UUID[]> => {
   const { rows } = await postgres.query<
     | PhotoAnnotationConfirmed
     | PhotoManuallyAnnotated
@@ -19,4 +20,41 @@ export const getPersonIdsForFaceId = async (faceId: UUID): Promise<UUID[]> => {
   )
 
   return Array.from(new Set(rows.map((row) => row.payload.personId)))
+}
+
+export const getPersonIdsForFaceId = async (faceId: UUID): Promise<UUID[]> => {
+  const annotationEvents = await getEventList<
+    | PhotoAnnotationConfirmed
+    | PhotoManuallyAnnotated
+    | UserConfirmedHisFace
+    | UserNamedPersonInPhoto
+    | UserRecognizedPersonInPhoto
+  >(
+    [
+      'PhotoAnnotationConfirmed',
+      'PhotoManuallyAnnotated',
+      'UserConfirmedHisFace',
+      'UserNamedPersonInPhoto',
+      'UserRecognizedPersonInPhoto',
+    ],
+    { faceId }
+  )
+
+  type PhotoId = UUID
+  type PersonId = UUID
+
+  const uniqueByPhotoId = new Map<PhotoId, PersonId>()
+  for (const annotationEvent of annotationEvents) {
+    const { photoId, personId } = annotationEvent.payload
+    uniqueByPhotoId.set(photoId, personId)
+  }
+
+  // Count how many photos of each person for this face (so the first 'try' is more probably the best)
+  const countByPersonId = new Map<PersonId, number>()
+  for (const personId of uniqueByPhotoId.values()) {
+    countByPersonId.set(personId, (countByPersonId.get(personId) || 0) + 1)
+  }
+  const personIdsSortedByPhotoCount = Array.from(countByPersonId.entries()).sort(([_, countA], [__, countB]) => countB - countA)
+
+  return personIdsSortedByPhotoCount.map(([personId, _]) => personId)
 }
