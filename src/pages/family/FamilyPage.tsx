9f4bf1ef8159fe03be3d62ab22f7977563a49249
Pierre Duchateau <pad@dabla.com>
2023-10-06 15:31:14 +0200

    On mobile: fix search panel that appears at the bottom

--- src/pages/family/FamilyPage.tsx
+++ src/pages/family/FamilyPage.tsx
@@ -837,7 +837,7 @@ function SearchPanel({
         </Transition.Child>
 
         <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
-          <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+          <div className='flex sm:min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
             <Transition.Child
               as={React.Fragment}
               enter='ease-out duration-300'
