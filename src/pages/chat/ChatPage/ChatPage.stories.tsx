9f7f0120e0a1468b838cc121b91e9bcdf4b060c4
Pierre Duchateau <pad@dabla.com>
2023-10-18 12:16:03 +0200

    Cleanup storybook

--- src/pages/chat/ChatPage/ChatPage.stories.tsx
+++ src/pages/chat/ChatPage/ChatPage.stories.tsx
@@ -25,6 +25,7 @@ export default {
             arePhotosEnabled: true,
             areThreadsEnabled: true,
             areVideosEnabled: true,
+            arePersonsEnabled: true,
           }}>
           <Story />
         </SessionContext.Provider>
@@ -36,79 +37,11 @@ export default {
 const t0 = Date.now()
 const HOUR = 3600 * 1000
 
-export const Empty = () => <ChatPage chatId={getUuid()} history={[]} />
-
-export const Basique = () => (
-  <ChatPage
-    chatId={getUuid()}
-    history={[
-      {
-        timestamp: t0 + 1 * HOUR,
-        type: 'message',
-        message: {
-          body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. Scelerisque amet elit non sit ut tincidunt condimentum. Nisl ultrices eu venenatis diam.',
-        },
-      },
-    ]}
-  />
-)
-
-export const AvecUnePhotoEtVisagesNonIdentifiés = () => (
-  <ChatPage
-    chatId={getUuid()}
-    history={[
-      {
-        timestamp: t0,
-        type: 'photo',
-        chatId: getUuid(),
-        chatId: getUuid(),
-        photoId: 'photo123' as UUID,
-        url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=500&h=500&q=80',
-        personsInPhoto: ['Jean', 'Marie', 'Philippe', 'Joseph', 'Helicopter', 'Banane'],
-        unrecognizedFacesInPhoto: 3,
-      },
-    ]}
-  />
-)
-
-export const AvecUnePhotoEtVisagesTousIdentifiés = () => (
-  <ChatPage
-    chatId={getUuid()}
-    history={[
-      {
-        timestamp: t0,
-        type: 'photo',
-        chatId: getUuid(),
-        photoId: 'photo123' as UUID,
-        url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=500&h=500&q=80',
-        personsInPhoto: ['Jean', 'Marie', 'Philippe', 'Joseph', 'Helicopter', 'Banane'],
-        unrecognizedFacesInPhoto: 0,
-      },
-    ]}
-  />
-)
-
-export const AvecUnePhotoEtUneDescription = () => (
-  <ChatPage
-    chatId={getUuid()}
-    history={[
-      {
-        timestamp: t0,
-        type: 'photo',
-        photoId: 'photo123' as UUID,
-        url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=500&h=500&q=80',
-        description: 'Cette photo commence a me taper sur les nerfs.',
-        personsInPhoto: ['Jean', 'Marie', 'Philippe', 'Joseph', 'Helicopter', 'Banane'],
-        unrecognizedFacesInPhoto: 3,
-      },
-    ]}
-  />
-)
-
 export const AvecUnMelangeDePhotoEtMessage = () => (
   <ChatPage
-    chatId={'01HB84HGH09ENWGWMQ0RB8DDMQ' as UUID}
-    lastUpdated={1695712152310 as Epoch}
+    chatId={getUuid()}
+    lastUpdated={t0 as Epoch}
+    title='Ceci est le titre'
     contentAsJSON={{
       type: 'doc',
       content: [
@@ -121,50 +54,46 @@ export const AvecUnMelangeDePhotoEtMessage = () => (
             },
           ],
         },
-        // {
-        //   type: 'photoNode',
-        //   attrs: {
-        //     chatId: getUuid(),
-        //     photoId: 'photo123' as UUID,
-        //     url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=500&h=500&q=80',
-        //     description: 'Ici la description',
-        //     personsInPhoto: encodeURIComponent(JSON.stringify(['Jean', 'Marie', 'Philippe', 'Joseph', 'Helicopter', 'Banane'])),
-        //     unrecognizedFacesInPhoto: 0,
-        //   },
-        // },
-        // {
-        //   type: 'paragraph',
-        //   content: [
-        //     {
-        //       type: 'text',
-        //       text: 'Second message',
-        //     },
-        //   ],
-        // },
-        // {
-        //   type: 'paragraph',
-        //   content: [
-        //     {
-        //       type: 'text',
-        //       text: 'Third message',
-        //     },
-        //   ],
-        // },
-        // {
-        //   type: 'photoNode',
-        //   attrs: {
-        //     chatId: getUuid(),
-        //     photoId: 'photo123' as UUID,
-        //     url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=500&h=500&q=80',
-        //     description: '',
-        //     personsInPhoto: encodeURIComponent(JSON.stringify([])),
-        //     unrecognizedFacesInPhoto: 10,
-        //   },
-        // },
-        // {
-        //   type: 'paragraph',
-        //   content: [],
-        // },
+        {
+          type: 'photoNode',
+          attrs: {
+            chatId: getUuid(),
+            photoId: 'photo123' as UUID,
+            url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=500&h=500&q=80',
+            description: 'Ici la description',
+            personsInPhoto: encodeURIComponent(JSON.stringify(['Jean', 'Marie', 'Philippe', 'Joseph', 'Helicopter', 'Banane'])),
+            unrecognizedFacesInPhoto: 0,
+          },
+        },
+        {
+          type: 'paragraph',
+          content: [
+            {
+              type: 'text',
+              text: 'Second message',
+            },
+          ],
+        },
+        {
+          type: 'paragraph',
+          content: [
+            {
+              type: 'text',
+              text: 'Third message',
+            },
+          ],
+        },
+        {
+          type: 'photoNode',
+          attrs: {
+            chatId: getUuid(),
+            photoId: 'photo123' as UUID,
+            url: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=500&h=500&q=80',
+            description: '',
+            personsInPhoto: encodeURIComponent(JSON.stringify([])),
+            unrecognizedFacesInPhoto: 10,
+          },
+        },
       ],
     }}
   />
