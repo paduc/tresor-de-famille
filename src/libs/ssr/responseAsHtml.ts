7e7db841c1199ea0fba267f23813cb0ff55d4934
Pierre Duchateau <pad@dabla.com>
2023-11-21 15:31:35 +0100

    Fix static head assets hrefs (make them absolute)

--- src/libs/ssr/responseAsHtml.ts
+++ src/libs/ssr/responseAsHtml.ts
@@ -46,19 +46,19 @@ export async function responseAsHtml(
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width,initial-scale=1" />
-          <link rel="apple-touch-icon-precomposed" sizes="57x57" href="apple-touch-icon-57x57.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114x114.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72x72.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144x144.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="60x60" href="apple-touch-icon-60x60.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="120x120" href="apple-touch-icon-120x120.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="76x76" href="apple-touch-icon-76x76.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152.png" />
-          <link rel="icon" type="image/png" href="favicon-196x196.png" sizes="196x196" />
-          <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96" />
-          <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
-          <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
-          <link rel="icon" type="image/png" href="favicon-128.png" sizes="128x128" />
+          <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/apple-touch-icon-57x57.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114x114.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72x72.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/apple-touch-icon-144x144.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/apple-touch-icon-60x60.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/apple-touch-icon-120x120.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/apple-touch-icon-76x76.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/apple-touch-icon-152x152.png" />
+          <link rel="icon" type="image/png" href="/favicon-196x196.png" sizes="196x196" />
+          <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+          <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+          <link rel="icon" type="image/png" href="/favicon-128.png" sizes="128x128" />
           <meta name="application-name" content="Trésor de famille" />
           <meta name="msapplication-TileColor" content="#FFFFFF" />
           <meta name="msapplication-TileImage" content="mstile-144x144.png" />
@@ -146,6 +146,8 @@ async function getSession(request: Request): Promise<Session> {
       })
     }
 
+    const hasFamiliesOtherThanDefault = userFamilies.some((f) => f.familyId !== (userId as string as FamilyId))
+
     const searchKey = makeSearchKey(
       user.id,
       userFamilies.map((f) => f.familyId)
@@ -162,6 +164,7 @@ async function getSession(request: Request): Promise<Session> {
       userId: user.id,
       personId: personInCurrentFamily?.personId,
       userFamilies: userFamilies.map(({ familyId, familyName, about }) => ({ familyId, familyName, about })),
+      hasFamiliesOtherThanDefault,
       currentFamilyId,
       searchKey,
       isAdmin: userId === ADMIN_USERID,
