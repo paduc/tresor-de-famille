b241a0b285e8bed65e63be2abe78aec8aff2f477
Pierre Duchateau <pad@dabla.com>
2023-11-23 15:49:18 +0100

    WIP: Clone thread assets as well (photos, persons) (Test)

---
+++ src/pages/thread/ThreadPage/PhotoClonedForSharing.ts
@@ -0,0 +1,34 @@
+import { DomainEvent, makeDomainEvent } from '../../../dependencies/DomainEvent'
+import { AppUserId } from '../../../domain/AppUserId'
+import { FaceId } from '../../../domain/FaceId'
+import { FamilyId } from '../../../domain/FamilyId'
+import { PersonId } from '../../../domain/PersonId'
+import { PhotoId } from '../../../domain/PhotoId'
+import { ThreadId } from '../../../domain/ThreadId'
+
+export type PhotoClonedForSharing = DomainEvent<
+  'PhotoClonedForSharing',
+  {
+    userId: AppUserId
+    familyId: FamilyId
+
+    // The new photoId
+    photoId: PhotoId
+
+    // TODO: set photo info
+    faces: { faceId: FaceId; personId?: PersonId; isIgnored?: boolean }[]
+    caption?: string
+
+    // The cloned thread the photo is inside of
+    threadId: ThreadId
+
+    clonedFrom: {
+      familyId: FamilyId
+      // the original photoId
+      photoId: PhotoId
+      threadId: ThreadId
+    }
+  }
+>
+
+export const PhotoClonedForSharing = makeDomainEvent<PhotoClonedForSharing>('PhotoClonedForSharing')
