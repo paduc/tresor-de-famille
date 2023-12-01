9dd0e5a5b9263dec08924c4d4c06dd45eecfa4c5
Pierre Duchateau <pad@dabla.com>
2023-12-01 12:51:10 +0100

    Refactor generic queries for persons, faces and families

--- src/pages/_getPersonById.ts
+++ src/pages/_getPersonById.ts
@@ -1,38 +1,25 @@
-import { postgres } from '../dependencies/database'
-import { getSingleEvent } from '../dependencies/getSingleEvent'
 import { PersonId } from '../domain/PersonId'
-import { UserNamedPersonInPhoto } from '../events/onboarding/UserNamedPersonInPhoto'
-import { UserNamedThemself } from '../events/onboarding/UserNamedThemself'
 
-import { UserCreatedRelationshipWithNewPerson } from './family/UserCreatedRelationshipWithNewPerson'
-import { UserChangedPersonName } from './person/UserChangedPersonName'
-import { PersonClonedForSharing } from './share/PersonClonedForSharing'
+import { getPersonEvents } from './_getPersonEvents'
 
 export type PersonById = { name: string }
 
 export const getPersonById = async ({ personId }: { personId: PersonId }): Promise<PersonById | null> => {
-  const { rows: personsAddedWithNewRelationship } = await postgres.query<UserCreatedRelationshipWithNewPerson>(
-    "SELECT * FROM history WHERE type = 'UserCreatedRelationshipWithNewPerson' AND payload->'newPerson'->>'personId'=$1",
-    [personId]
-  )
+  const personEvents = await getPersonEvents(personId)
 
-  let name = personsAddedWithNewRelationship[0]?.payload.newPerson.name
+  if (!personEvents.length) return null
 
-  const userNamedEvent = await getSingleEvent<
-    UserNamedThemself | UserNamedPersonInPhoto | UserChangedPersonName | PersonClonedForSharing
-  >(['UserNamedThemself', 'UserNamedPersonInPhoto', 'UserChangedPersonName', 'PersonClonedForSharing'], {
-    personId,
-  })
+  const latestEvent = personEvents.at(-1)!
 
-  if (userNamedEvent) {
-    name = userNamedEvent.payload.name
+  switch (latestEvent.type) {
+    case 'UserCreatedRelationshipWithNewPerson':
+      return { name: latestEvent.payload.newPerson.name }
+    case 'PersonClonedForSharing':
+    case 'UserChangedPersonName':
+    case 'UserNamedPersonInPhoto':
+    case 'UserNamedThemself':
+      return { name: latestEvent.payload.name }
   }
-
-  if (name) {
-    return { name }
-  }
-
-  return null
 }
 
 export const getPersonByIdOrThrow = async ({ personId }: { personId: PersonId }): Promise<PersonById> => {
