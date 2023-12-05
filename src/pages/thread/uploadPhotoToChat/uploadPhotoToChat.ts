22a2a3ab9002dd6ebb6b48ae42dcbf804179b396
Pierre Duchateau <pad@dabla.com>
2023-12-05 19:32:25 +0100

    Add familyId to UploadPhotoToChat

--- src/pages/thread/uploadPhotoToChat/uploadPhotoToChat.ts
+++ src/pages/thread/uploadPhotoToChat/uploadPhotoToChat.ts
@@ -5,6 +5,7 @@ import { AppUserId } from '../../../domain/AppUserId'
 import { PhotoId } from '../../../domain/PhotoId'
 import { ThreadId } from '../../../domain/ThreadId'
 import { UserUploadedPhotoToChat } from './UserUploadedPhotoToChat'
+import { FamilyId } from '../../../domain/FamilyId'
 
 type UploadPhotoToChatArgs = {
   file: Express.Multer.File
@@ -17,5 +18,7 @@ export async function uploadPhotoToThread({ file, photoId, threadId, userId }: U
 
   const location = await uploadPhoto({ contents: fs.createReadStream(originalPath), id: photoId })
 
-  await addToHistory(UserUploadedPhotoToChat({ chatId: threadId, photoId, location, uploadedBy: userId }))
+  await addToHistory(
+    UserUploadedPhotoToChat({ chatId: threadId, photoId, location, uploadedBy: userId, familyId: userId as string as FamilyId })
+  )
 }
