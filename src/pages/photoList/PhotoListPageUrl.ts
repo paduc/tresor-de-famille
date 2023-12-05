fd6b4b0c4bd3d642391b0777d12d58d1f78e98e4
Pierre Duchateau <pad@dabla.com>
2023-12-05 19:24:50 +0100

    PhotoListPage: Ajouter switch de famille

--- src/pages/photoList/PhotoListPageUrl.ts
+++ src/pages/photoList/PhotoListPageUrl.ts
@@ -1,1 +1,9 @@
+import { FamilyId } from '../../domain/FamilyId'
+
 export const PhotoListPageUrl = '/photos.html'
+export const PhotoListPageUrlWithFamily = (familyId?: FamilyId) => {
+  if (familyId) {
+    return `/${familyId}/photos.html`
+  }
+  return '(/:familyId)/photos.html'
+}
