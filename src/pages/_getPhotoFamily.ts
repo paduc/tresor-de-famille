11ba92b5512fa8a118af58b96398544ad09af76d
Pierre Duchateau <pad@dabla.com>
2023-12-08 14:51:57 +0100

    In PhotoPage, annotate faces with person in the same family as photo

--- src/pages/_getPhotoFamily.ts
+++ src/pages/_getPhotoFamily.ts
@@ -1,32 +1,25 @@
 import { FamilyId } from '../domain/FamilyId'
 import { PhotoId } from '../domain/PhotoId'
-import { PhotoClonedForSharing } from './thread/ThreadPage/PhotoClonedForSharing'
 import { getPhotoEvents } from './_getPhotoEvents'
 
-export async function getPhotoFamily(photoId: PhotoId): Promise<FamilyId | null> {
+export async function getPhotoFamilyId(photoId: PhotoId): Promise<FamilyId> {
   const photoEvents = await getPhotoEvents(photoId)
 
-  if (!photoEvents.length) return null
+  if (!photoEvents.length) {
+    throw new Error('Cannot get family because photo does not exist')
+  }
 
-  // If there is a cloned event, it gives us the family
-  const cloneEvent = photoEvents.find((e): e is PhotoClonedForSharing => e.type === 'PhotoClonedForSharing')
-  if (cloneEvent) return cloneEvent.payload.familyId
+  const creationEvent = photoEvents.at(0)!
 
-  const creationEvent = photoEvents.at(0)
-  if (!creationEvent) return null
-
-  // If not cloned, the familyId is the userId
   switch (creationEvent.type) {
     case 'OnboardingUserUploadedPhotoOfFamily':
     case 'OnboardingUserUploadedPhotoOfThemself':
-      return creationEvent.payload.uploadedBy as string as FamilyId
     case 'PhotoClonedForSharing':
-    case 'UserDeletedPhoto':
     case 'UserInsertedPhotoInRichTextThread':
-      return creationEvent.payload.userId as string as FamilyId
-    case 'UserAddedCaptionToPhoto':
-      return creationEvent.payload.addedBy as string as FamilyId
     case 'UserUploadedPhotoToChat':
-      return creationEvent.payload.uploadedBy as string as FamilyId
+      return creationEvent.payload.familyId
+    case 'UserDeletedPhoto':
+    case 'UserAddedCaptionToPhoto':
+      throw new Error('First event for this photo is not possible')
   }
 }
