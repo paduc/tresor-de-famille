f00305bb7cf1ae9bd022878ffe1ae13987d9afc5
Pierre Duchateau <pad@dabla.com>
2024-01-18 19:29:15 +0100

    Add forgotten share events

--- src/pages/_getFamiliesWithAccessToPerson.ts
+++ src/pages/_getFamiliesWithAccessToPerson.ts
@@ -2,17 +2,18 @@ import { getEventList } from '../dependencies/getEventList'
 import { FamilyId } from '../domain/FamilyId'
 import { PersonId } from '../domain/PersonId'
 import { getOriginalPersonFamily } from './_getOriginalPersonFamily'
+import { PersonAutoShareWithFamilyCreation } from './share/PersonAutoShareWithFamilyCreation'
 import { PersonAutoSharedWithPhotoFace } from './share/PersonAutoSharedWithPhotoFace'
+import { PersonAutoSharedWithRelationship } from './share/PersonAutoSharedWithRelationship'
 
 export async function getFamiliesWithAccessToPerson({ personId }: { personId: PersonId }): Promise<FamilyId[]> {
   const familyIds = new Set<FamilyId>()
 
   familyIds.add(await getOriginalPersonFamily(personId))
 
-  const shareEvents = await getEventList<PersonAutoSharedWithPhotoFace | PersonAutoSharedWithPhotoFace>(
-    ['PersonAutoSharedWithPhotoFace', 'PersonAutoSharedWithPhotoFace'],
-    { personId }
-  )
+  const shareEvents = await getEventList<
+    PersonAutoSharedWithPhotoFace | PersonAutoShareWithFamilyCreation | PersonAutoSharedWithRelationship
+  >(['PersonAutoSharedWithPhotoFace', 'PersonAutoShareWithFamilyCreation', 'PersonAutoSharedWithRelationship'], { personId })
 
   for (const shareEvent of shareEvents) {
     familyIds.add(shareEvent.payload.familyId)
--- src/pages/_isPersonSharedWithFamily.ts
+++ src/pages/_isPersonSharedWithFamily.ts
@@ -2,7 +2,9 @@ import { getEventList } from '../dependencies/getEventList'
 import { FamilyId } from '../domain/FamilyId'
 import { PersonId } from '../domain/PersonId'
 import { getOriginalPersonFamily } from './_getOriginalPersonFamily'
+import { PersonAutoShareWithFamilyCreation } from './share/PersonAutoShareWithFamilyCreation'
 import { PersonAutoSharedWithPhotoFace } from './share/PersonAutoSharedWithPhotoFace'
+import { PersonAutoSharedWithRelationship } from './share/PersonAutoSharedWithRelationship'
 
 export async function isPersonSharedWithFamily({
   personId,
@@ -17,10 +19,9 @@ export async function isPersonSharedWithFamily({
     return true
   }
 
-  const shareEvents = await getEventList<PersonAutoSharedWithPhotoFace | PersonAutoSharedWithPhotoFace>(
-    ['PersonAutoSharedWithPhotoFace', 'PersonAutoSharedWithPhotoFace'],
-    { personId }
-  )
+  const shareEvents = await getEventList<
+    PersonAutoSharedWithPhotoFace | PersonAutoSharedWithRelationship | PersonAutoShareWithFamilyCreation
+  >(['PersonAutoSharedWithPhotoFace', 'PersonAutoSharedWithRelationship', 'PersonAutoShareWithFamilyCreation'], { personId })
 
   for (const shareEvent of shareEvents) {
     if (shareEvent.payload.familyId === familyId) {
