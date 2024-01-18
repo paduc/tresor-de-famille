f665af5ca7694c14ba0f42c74bb2b7d4f22caaca
Pierre Duchateau <pad@dabla.com>
2024-01-18 14:54:09 +0100

    getUserFamilies returns only unique families

--- src/pages/_getUserFamilies.ts
+++ src/pages/_getUserFamilies.ts
@@ -4,47 +4,55 @@ import { FamilyId } from '../domain/FamilyId'
 import { FamilyShareCode } from '../domain/FamilyShareCode'
 import { UserRegisteredWithInvitation } from '../events/UserRegisteredWithInvitation'
 import { FamilyColorCodes } from '../libs/ssr/FamilyColorCodes'
+import { asFamilyId } from '../libs/typeguards'
 import { getFamilyById } from './_getFamilyById'
 import { UserAcceptedInvitation } from './invitation/UserAcceptedInvitation'
 import { UserCreatedNewFamily } from './share/UserCreatedNewFamily'
 
-export const getUserFamilies = async (
-  userId: AppUserId
-): Promise<
-  {
-    familyId: FamilyId
-    familyName: string
-    about: string
-    shareCode: FamilyShareCode
-    isRegistrationFamily: boolean
-    isCreator: boolean
-    color: string
-  }[]
-> => {
+type Family = {
+  familyId: FamilyId
+  familyName: string
+  about: string
+  shareCode: FamilyShareCode
+  isRegistrationFamily: boolean
+  isCreator: boolean
+  color: string
+}
+
+/**
+ * getUserFamilies (including his own space)
+ * @param userId
+ * @returns a list of unique families, including the user default family
+ */
+export const getUserFamilies = async (userId: AppUserId): Promise<Family[]> => {
   // Personnel space
-  const userFamilies = [
-    {
-      familyId: userId as string as FamilyId,
-      familyName: 'Votre espace Personnel',
-      about: '',
-      shareCode: '' as string as FamilyShareCode,
-      isRegistrationFamily: false,
-      isCreator: false,
-    },
-  ]
+  const userFamilies = new Map<FamilyId, Family>()
+
+  userFamilies.set(asFamilyId(userId), {
+    familyId: asFamilyId(userId),
+    familyName: 'Votre espace Personnel',
+    about: '',
+    shareCode: '' as string as FamilyShareCode,
+    isRegistrationFamily: false,
+    isCreator: false,
+    color: '',
+  })
 
   const userCreatedFamilyEvents = await getEventList<UserCreatedNewFamily>('UserCreatedNewFamily', { userId })
 
-  userFamilies.push(
-    ...userCreatedFamilyEvents.map(({ payload: { familyId, familyName, about, shareCode } }) => ({
+  for (const {
+    payload: { familyId, familyName, about, shareCode },
+  } of userCreatedFamilyEvents) {
+    userFamilies.set(familyId, {
       familyId,
       familyName,
       about,
       shareCode,
       isRegistrationFamily: false,
       isCreator: true,
-    }))
-  )
+      color: '',
+    })
+  }
 
   const acceptedInvitationEvents = await getEventList<UserAcceptedInvitation | UserRegisteredWithInvitation>(
     ['UserAcceptedInvitation', 'UserRegisteredWithInvitation'],
@@ -53,21 +61,25 @@ export const getUserFamilies = async (
   for (const acceptedInvitationEvent of acceptedInvitationEvents) {
     const { familyId, shareCode } = acceptedInvitationEvent.payload
 
+    if (userFamilies.has(familyId)) {
+      continue
+    }
+
     const family = await getFamilyById(familyId)
 
     if (family) {
       const { name, about } = family
-
-      userFamilies.push({
+      userFamilies.set(familyId, {
         familyId,
         familyName: name,
         about,
         shareCode,
         isRegistrationFamily: acceptedInvitationEvent.type === 'UserRegisteredWithInvitation',
         isCreator: false,
+        color: '',
       })
     }
   }
 
-  return userFamilies.map((userFamily, index) => ({ ...userFamily, color: FamilyColorCodes[index + 1] }))
+  return Array.from(userFamilies).map(([_, userFamily], index) => ({ ...userFamily, color: FamilyColorCodes[index + 1] }))
 }
