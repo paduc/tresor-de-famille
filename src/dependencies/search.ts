7680c8399ce7044ac325d8ec1eace5dfd2440c00
Pierre Duchateau <pad@dabla.com>
2024-01-24 14:17:29 +0100

    Change how algolia index is updated

--- src/dependencies/search.ts
+++ src/dependencies/search.ts
@@ -2,6 +2,7 @@ import algoliasearch from 'algoliasearch'
 import { AppUserId } from '../domain/AppUserId'
 import { ALGOLIA_SEARCHKEY } from './env'
 import { FamilyId } from '../domain/FamilyId'
+import { PersonId } from '../domain/PersonId'
 
 const ALGOLIA_APPID = process.env.ALGOLIA_APPID
 const ALGOLIA_APPKEY = process.env.ALGOLIA_APPKEY
@@ -20,9 +21,40 @@ personsIndex.setSettings({
 })
 
 export const makeSearchKey = (userId: AppUserId, familyIds: FamilyId[]) => {
-  const filters = [`visible_by:user/${userId}`, ...familyIds.map((familyId) => `visible_by:family/${familyId}`)].join(' OR ')
+  const filters = [`visible_by:family/${userId}`, ...familyIds.map((familyId) => `visible_by:family/${familyId}`)].join(' OR ')
 
   return searchClient.generateSecuredApiKey(ALGOLIA_SEARCHKEY, {
     filters,
   })
+}
+
+export async function addFamilyVisibilityToIndex({ personId, familyId }: { personId: PersonId; familyId: FamilyId }) {
+  try {
+    const obj = await personsIndex.getObject<{ visible_by: string[] }>(personId)
+    if (obj && Array.isArray(obj.visible_by) && !obj.visible_by.includes(`family/${familyId}`)) {
+      await personsIndex.partialUpdateObject({
+        objectID: personId,
+        visible_by: [...obj.visible_by, `family/${familyId}`],
+      })
+      return
+    }
+
+    await personsIndex.partialUpdateObject({
+      objectID: personId,
+      visible_by: [`family/${familyId}`],
+    })
+  } catch (error) {
+    console.error('addFamilyVisibilityToIndex: Could not add family to visibly_by')
+  }
+}
+
+export async function changePersonNameInIndex({ personId, name }: { personId: PersonId; name: string }) {
+  try {
+    await personsIndex.partialUpdateObject({
+      objectID: personId,
+      name,
+    })
+  } catch (error) {
+    console.error('Could not change persons name in algolia index', error)
+  }
 }
