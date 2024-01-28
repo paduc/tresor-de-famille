7b6a18e9a9153c1fa8ffaf6936c1097a730bc51a
Pierre Duchateau <pad@dabla.com>
2024-01-28 15:41:01 +0100

    Add failsafes on thumbnails

--- src/pages/photoApi/getPhotoThumbnail.route.ts
+++ src/pages/photoApi/getPhotoThumbnail.route.ts
@@ -1,4 +1,6 @@
 import sharp from 'sharp'
+import { createReadStream } from 'node:fs'
+import path from 'node:path'
 import { z } from 'zod'
 
 import { requireAuth } from '../../dependencies/authn'
@@ -17,17 +19,38 @@ pageRouter.route(ThumbnailURL(':photoId')).get(requireAuth(), async (request, re
     }
 
     // Get the original image as a Readable stream
-    const originalImageStream = downloadPhoto(photoId)
+    const originalImageStream = downloadPhoto(photoId).on('error', (err) => {
+      console.error(err)
+      throw new Error('Error during downloadPhoto')
+    })
 
-    const pipeline = sharp().resize(300, 300, { fit: 'cover' }).rotate() // to keep original orientation
+    const pipeline = sharp()
+      .resize(300, 300, { fit: 'cover' })
+      .rotate()
+      .on('error', (err) => {
+        console.error(err)
+        throw new Error('Error during sharp')
+      }) // to keep original orientation
 
-    originalImageStream.pipe(pipeline)
+    originalImageStream.pipe(pipeline).on('error', (err) => {
+      console.error(err)
+      throw new Error('Error during piping between originalImageStream and sharp')
+    })
 
     response.set('Content-Type', 'image/*')
     response.set('Cache-Control', 'private, max-age=15552000')
-    pipeline.pipe(response)
+    pipeline.pipe(response).on('error', (err) => {
+      console.error(err)
+      throw new Error('Error during piping to response')
+    })
   } catch (error) {
     console.error('getPhotoThumbnail', error)
-    response.status(500).send()
+
+    createReadStream(path.join(path.join(__dirname, '../../assets/error-thumbnail.png')))
+      .pipe(response)
+      .on('error', (err) => {
+        console.error(err)
+        throw new Error('error piping to placeholder')
+      })
   }
 })
