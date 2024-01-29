5395d8612525115c8eefe01f16614ed1dd6db7d0
Pierre Duchateau <pad@dabla.com>
2024-01-29 18:53:47 +0100

    Fix ProfilePicUrlForUser for UserRecognizedThemselfAsPerson

--- src/pages/_getProfilePicUrlForUser.ts
+++ src/pages/_getProfilePicUrlForUser.ts
@@ -1,10 +1,14 @@
 import { getSingleEvent } from '../dependencies/getSingleEvent'
 import { AppUserId } from '../domain/AppUserId'
 import { UserNamedThemself } from '../events/onboarding/UserNamedThemself'
+import { UserRecognizedThemselfAsPerson } from '../events/onboarding/UserRecognizedThemselfAsPerson'
 import { getProfilePicUrlForPerson } from './_getProfilePicUrlForPerson'
 
 export const getProfilePicUrlForUser = async (userId: AppUserId): Promise<string | null> => {
-  const person = await getSingleEvent<UserNamedThemself>(['UserNamedThemself'], { userId })
+  const person = await getSingleEvent<UserNamedThemself | UserRecognizedThemselfAsPerson>(
+    ['UserNamedThemself', 'UserRecognizedThemselfAsPerson'],
+    { userId }
+  )
 
   if (!person) return null
 
