6c93e68dc09eee78d17276354b43e66517c20492
Pierre Duchateau <pad@dabla.com>
2024-01-30 18:08:35 +0100

    Landing page: update logo and use brighter theme

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -9,7 +9,7 @@ type LandingPageProps = {}
 export const LandingPage = ({}: LandingPageProps) => {
   return (
     <BareLayout>
-      <div className='relative h-full isolate overflow-y-scroll overflow-x-hidden bg-indigo-950'>
+      <div className='relative h-full isolate overflow-y-scroll overflow-x-hidden bg-indigo-700'>
         <header className=''>
           <nav className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8' aria-label='Global'>
             <div className='flex flex-1 justify-end'>
@@ -24,7 +24,7 @@ export const LandingPage = ({}: LandingPageProps) => {
           className='absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]'
           aria-hidden='true'>
           <div
-            className='aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20'
+            className='aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#e879f9] to-[#4338ca] opacity-20'
             style={{
               clipPath:
                 'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
@@ -34,18 +34,15 @@ export const LandingPage = ({}: LandingPageProps) => {
         <div className='mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40'>
           <div className='mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8'>
             {/** LOGO */}
-            <div className='relative lg:-top-10 flex-col items-center'>
-              <img
-                src='images/logo-dark-transparent.png'
-                className='opacity-80 transform scale-75 lg:scale-100 relative -left-8 lg:left-0'
-              />
-              <div className='text-white text-opacity-80 uppercase tracking-wide text-xl font-bold relative -top-4 lg:top-0'>
+            <div className='relative lg:-top-10 flex items-center'>
+              <img src='images/tdf-picto-white-transparent.png' className='opacity-100 h-24 w-24 relative -left-3 lg:left-0' />
+              <div className='text-white text-opacity-100 uppercase tracking-wide text-xl font-bold relative -left-3 lg:left-0'>
                 Trésor de famille
               </div>
             </div>
             {/** Petits badges au dessus du slogan */}
             <div className='mt-12 sm:mt-12'>
-              <span className='rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-700 ring-1 ring-inset ring-indigo-500/20'>
+              <span className='rounded-full bg-white/10 px-3 py-1 text-sm leading-6 text-white/60 ring-1 ring-inset ring-white/20'>
                 En cours de construction
               </span>
               {/* <a href='#' className='inline-flex space-x-6'>
