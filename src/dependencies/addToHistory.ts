e97e5854bc1e32a8f4cbc4139b0b2db83b4fc6c4
Pierre Duchateau <pad@dabla.com>
2024-02-06 12:34:04 +0100

    Add workaround to make sure payloads dont contains a null char

--- src/dependencies/addToHistory.ts
+++ src/dependencies/addToHistory.ts
@@ -1,17 +1,25 @@
-import { DomainEvent } from './DomainEvent'
+import { DomainEvent, JSON as JSONType } from './DomainEvent'
 import { postgres } from './database'
 
 export const addToHistory = async ({ id, type, payload, occurredAt }: DomainEvent) => {
-  await postgres.query('INSERT INTO history (id, type, payload, "occurredAt") VALUES ($1, $2, $3, $4)', [
-    id,
-    type,
-    payload,
-    new Date(occurredAt),
-  ])
+  try {
+    await postgres.query('INSERT INTO history (id, type, payload, "occurredAt") VALUES ($1, $2, $3, $4)', [
+      id,
+      type,
+      removeNullChar(payload),
+      new Date(occurredAt),
+    ])
+  } catch (error) {
+    console.error('addToHistory failed', error)
+  }
 }
 
 export const createHistoryTable = async () => {
   return postgres.query(
     `CREATE TABLE IF NOT EXISTS history (id UUID PRIMARY KEY, type VARCHAR(255) NOT NULL, payload JSONB, "occurredAt" TIMESTAMPTZ NOT NULL);`
   )
+}
+
+function removeNullChar(payload: JSONType): JSONType {
+  return JSON.parse(JSON.stringify(payload).replace(/\\u0000/g, ''))
 }
