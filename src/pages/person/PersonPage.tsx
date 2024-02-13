23a9618222ce6e637a80a6568cfd4b1e88548dd7
Pierre Duchateau <pad@dabla.com>
2024-02-13 19:21:34 +0100

    PersonPage: limit the amount of photos displayed

--- src/pages/person/PersonPage.tsx
+++ src/pages/person/PersonPage.tsx
@@ -10,6 +10,7 @@ import { TDFModal } from '../_components/TDFModal'
 import { AppLayout } from '../_components/layout/AppLayout'
 import { ThumbnailURL } from '../photoApi/ThumbnailURL'
 import { PhotoPageUrl } from '../photo/PhotoPageUrl'
+import { useState } from 'react'
 
 // @ts-ignore
 function classNames(...classes) {
@@ -30,9 +31,13 @@ export type PersonPageProps = {
   }[]
 }
 
+const INITIAL_PHOTO_COUNT = 6
+const PHOTO_COUNT_STEP = 6
+
 export const PersonPage = withBrowserBundle(({ person, photos, alternateProfilePics }: PersonPageProps) => {
   const [isProfilePicOpen, openProfilePic] = React.useState<boolean>(false)
   const closeProfilePic = React.useCallback(() => openProfilePic(false), [])
+  const [photosToDisplayCount, setPhotosToDisplayCount] = useState<number>(INITIAL_PHOTO_COUNT)
 
   const [isNameChangerOpen, openNameChanger] = React.useState<boolean>(false)
   const closeNameChanger = React.useCallback(() => openNameChanger(false), [])
@@ -123,10 +128,11 @@ export const PersonPage = withBrowserBundle(({ person, photos, alternateProfileP
         </div>
       ) : null} */}
 
-      <div className='bg-white p-6'>
-        <h3 className='text-lg font-medium leading-6 text-gray-900'>Photos de {person.name}</h3>
+      {photos.length > 0 ? (
+        <div className='bg-white p-6'>
+          <h3 className='text-lg font-medium leading-6 text-gray-900'>Photos où {person.name} apparaît</h3>
 
-        {/*<InlinePhotoUploadBtn
+          {/*<InlinePhotoUploadBtn
           formAction='/add-photo.html'
           hiddenFields={{ familyId: person.familyId }}
           formKey={`uploadFamily${person.familyId}`}>
@@ -135,21 +141,45 @@ export const PersonPage = withBrowserBundle(({ person, photos, alternateProfileP
           </span>
     </InlinePhotoUploadBtn>*/}
 
-        <ul role='list' className='grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 mt-3'>
-          {photos.map((photo) => (
-            <li key={photo.photoId} className='relative'>
-              <div className='group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100'>
-                <img src={photo.url} alt='' className='pointer-events-none object-cover group-hover:opacity-75' />
-                <a
-                  href={`${PhotoPageUrl(photo.photoId)}?profileId=${person.personId}`}
-                  className='absolute inset-0 focus:outline-none'>
-                  <span className='sr-only'>Voir la photo</span>
-                </a>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+          <ul
+            role='list'
+            className='grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 lg:max-w-4xl mt-3'>
+            {photos.slice(0, photosToDisplayCount).map((photo) => (
+              <li key={photo.photoId} className='relative'>
+                <div className='group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100'>
+                  <img src={photo.url} alt='' className='pointer-events-none object-cover group-hover:opacity-75' />
+                  <a
+                    href={`${PhotoPageUrl(photo.photoId)}?profileId=${person.personId}`}
+                    className='absolute inset-0 focus:outline-none'>
+                    <span className='sr-only'>Voir la photo</span>
+                  </a>
+                </div>
+              </li>
+            ))}
+          </ul>
+          {photos.length > photosToDisplayCount ? (
+            <div className='mt-3'>
+              <button
+                className={`${secondaryButtonStyles}`}
+                onClick={() => {
+                  setPhotosToDisplayCount((count) => count + Math.min(PHOTO_COUNT_STEP, photos.length - count))
+                }}>
+                Afficher plus de photos
+              </button>
+            </div>
+          ) : (
+            <div className='mt-3'>
+              <button
+                className={`${secondaryButtonStyles}`}
+                onClick={() => {
+                  setPhotosToDisplayCount(INITIAL_PHOTO_COUNT)
+                }}>
+                Afficher moins
+              </button>
+            </div>
+          )}
+        </div>
+      ) : null}
 
       <ProfilePictureSelector
         isOpen={isProfilePicOpen}
