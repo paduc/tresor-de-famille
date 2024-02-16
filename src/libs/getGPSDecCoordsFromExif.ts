f3c4da441d78ab5ec1530eaa6508d0eb301f011b
Pierre Duchateau <pad@dabla.com>
2024-02-16 16:16:01 +0100

    Fix GPS from EXIF for case of Number (!=number)

--- src/libs/getGPSDecCoordsFromExif.ts
+++ src/libs/getGPSDecCoordsFromExif.ts
@@ -11,9 +11,9 @@ export function getGPSDecCoordsFromExif(exif: EXIF): { lat: number; long: number
       GPSLatitudeRef,
     } = z
       .object({
-        GPSLongitude: z.tuple([z.number(), z.number(), z.number()]),
+        GPSLongitude: z.tuple([z.coerce.number(), z.coerce.number(), z.coerce.number()]),
         GPSLongitudeRef: z.union([z.literal('N'), z.literal('W'), z.literal('E'), z.literal('S')]),
-        GPSLatitude: z.tuple([z.number(), z.number(), z.number()]),
+        GPSLatitude: z.tuple([z.coerce.number(), z.coerce.number(), z.coerce.number()]),
         GPSLatitudeRef: z.union([z.literal('N'), z.literal('W'), z.literal('E'), z.literal('S')]),
       })
       .parse(exif)
@@ -24,12 +24,16 @@ export function getGPSDecCoordsFromExif(exif: EXIF): { lat: number; long: number
     return { lat, long }
   } catch (error) {
     if (Object.keys(exif).some((key) => key.startsWith('GPS'))) {
+      // console.error(error)
+
       console.error(
         'getGPSDecCoordsFromExif received malformed GPS coords',
         JSON.stringify(
           Object.entries(exif)
             .filter(([key, value]) => key.startsWith('GPS'))
-            .reduce((gpsValues, [key, value]) => ({ ...gpsValues, [key]: value }), {})
+            .reduce((gpsValues, [key, value]) => ({ ...gpsValues, [key]: value }), {}),
+          null,
+          2
         )
       )
     }
