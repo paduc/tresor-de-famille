c53c163c1c4b251de6f74fe25336c3b3721b0e45
Pierre Duchateau <pad@dabla.com>
2024-02-16 16:16:16 +0100

    Remove "undefined" property in Exif

--- src/pages/photoApi/getExif.ts
+++ src/pages/photoApi/getExif.ts
@@ -4,6 +4,18 @@ import path from 'node:path'
 import { findEXIFinJPEG, findEXIFinHEIC, EXIF } from '../../libs/exif'
 
 export function getExif(file: Express.Multer.File): EXIF | undefined {
+  const exifRaw = getExifRaw(file)
+
+  if (!exifRaw) return
+
+  // Remove the "undefined" key value
+  // @ts-ignore
+  const { undefined, ...exif } = exifRaw
+
+  return exif
+}
+
+function getExifRaw(file: Express.Multer.File): EXIF | undefined {
   try {
     const { path: originalPath, originalname } = file
     const extension = path.extname(originalname).trim().toLowerCase()
