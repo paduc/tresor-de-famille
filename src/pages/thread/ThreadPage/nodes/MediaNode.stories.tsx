705cce6f2b4d4fa01c375c8cda1de8528f65da44
Pierre Duchateau <pad@dabla.com>
2024-03-28 11:25:39 +0100

    Tweak mediaNode for different status

---
+++ src/pages/thread/ThreadPage/nodes/MediaNode.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { MediaNodeItemComponent } from './MediaNode'
+import { MediaId } from '../../../../domain/MediaId'
+import { MediaStatus } from '../../../media/MediaStatus'
+
+export default { title: 'MediaNode', component: MediaNodeItemComponent, parameters: { layout: 'fullscreen' } }
+
+const fakeVideoUrl =
+  'https://iframe.mediadelivery.net/embed/222778/1f5d2e38-c4f7-435f-b024-1ecbd3bc7bb9?autoplay=true&loop=false&muted=false&preload=true&responsive=true'
+
+function MediaNodeItemComponentByStatus(mediaStatus: MediaStatus) {
+  return () => (
+    <div className='w-full sm:ml-6 max-w-2xl pt-3 pb-40'>
+      <div className='divide-y divide-gray-200 overflow-hidden sm:rounded-lg bg-white shadow'>
+        <div className='px-4 sm:px-0 py-4 text-gray-800 text-lg  whitespace-pre-wrap [&+p]:-mt-1 [&+p]:border-t-0 [&+p]:pt-0'>
+          <div className='sm:ml-6 max-w-2xl relative'>
+            <MediaNodeItemComponent
+              mediaId={'mediaId' as MediaId}
+              url={fakeVideoUrl}
+              latestCaption={undefined}
+              removeMedia={() => alert('removeMedia')}
+              mediaStatus={mediaStatus}
+              autosaveStatus='idle'
+              saveNewCaption={() => alert('saveNewCaption')}
+              onCaptionChange={(e) => alert(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export const Created = MediaNodeItemComponentByStatus(0)
+export const Uploaded = MediaNodeItemComponentByStatus(1)
+export const Processing = MediaNodeItemComponentByStatus(2)
+export const Transcoding = MediaNodeItemComponentByStatus(3)
+export const Finished = MediaNodeItemComponentByStatus(4)
+export const Error = MediaNodeItemComponentByStatus(5)
+export const UploadFailed = MediaNodeItemComponentByStatus(6)
+export const NotFound = MediaNodeItemComponentByStatus(404)
--- src/pages/thread/ThreadPage/nodes/MediaNode.tsx
+++ src/pages/thread/ThreadPage/nodes/MediaNode.tsx
@@ -1,4 +1,4 @@
-import { TrashIcon } from '@heroicons/react/24/outline'
+import { TrashIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Node } from '@tiptap/core'
 import { Attributes, NodeViewWrapper, ReactNodeViewRenderer, mergeAttributes } from '@tiptap/react'
 import axios from 'axios'
@@ -175,11 +175,12 @@ const MediaItem = (props: MediaItemProps) => {
       onCaptionChange={(e) => {
         debouncedSaveNewCaption(e.target.value)
       }}
+      saveNewCaption={saveNewCaption}
     />
   )
 }
 
-function MediaNodeItemComponent({
+export function MediaNodeItemComponent({
   mediaId,
   url,
   latestCaption,
@@ -214,38 +215,82 @@ function MediaNodeItemComponent({
         </div>
       </div>
 
-      <div>
-        <div className='mb-2'>
-          <div style={{ position: 'relative', paddingTop: '56.25%' }}>
-            {mediaStatus === 4 ? (
-              <iframe
-                src={url}
-                loading='lazy'
-                style={{ border: 0, position: 'absolute', top: 0, height: '100%', width: '100%' }}
-                allow='accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;'
-                allowFullScreen={true}
-              />
-            ) : (
-              <div>Not ready {MediaStatus[mediaStatus]}</div>
-            )}
-          </div>
-        </div>
-
-        <div className='w-full pr-10'>
-          <div className='inline-flex my-3 mr-10 items-center w-full'>
-            <TextareaAutosize
-              minRows={1}
-              className='flex-1 text-md text-gray-600 whitespace-pre-wrap placeholder:italic border-none p-0 ring-0 focus:ring-0'
-              placeholder='Cliquer ici pour ajouter une légende'
-              defaultValue={latestCaption || ''}
-              onChange={onCaptionChange}
-            />
-            <div className='flex-0 h-6 w-8'>
-              <StatusIndicator status={autosaveStatus} />
+      <div className='w-full pr-4'>
+        {mediaStatus === 4 ? (
+          <iframe
+            src={url}
+            loading='lazy'
+            className='w-full h-44 sm:h-96 border rounded-md border-gray-300 shadow-sm'
+            // style={{ border: 0, position: 'absolute', top: 0, height: '100%', width: '100%' }}
+            allow='accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;'
+            allowFullScreen={true}
+          />
+        ) : (
+          <div className='w-full h-44 sm:h-96  flex items-center justify-center bg-gray-100 rounded-md'>
+            <div className='flex items-center gap-2 justify-items-center'>
+              {[5, 6, 404].includes(mediaStatus) ? (
+                <>
+                  <XMarkIcon className='h-8 w-8 text-red-600' />
+                  <div className=''>
+                    {MessageByStatus[mediaStatus]}
+                    <span className='sr-only'>Loading...</span>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <svg
+                    aria-hidden='true'
+                    className='block w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
+                    viewBox='0 0 100 101'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'>
+                    <path
+                      d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+                      fill='currentColor'
+                    />
+                    <path
+                      d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+                      fill='currentFill'
+                    />
+                  </svg>
+                  <div className=''>
+                    {MessageByStatus[mediaStatus]}
+                    <span className='sr-only'>Loading...</span>
+                  </div>
+                </>
+              )}
             </div>
           </div>
-        </div>
+        )}
+
+        {mediaStatus === 4 ? (
+          <div className='w-full pr-10'>
+            <div className='inline-flex my-3 mr-10 items-center w-full'>
+              <TextareaAutosize
+                minRows={1}
+                className='flex-1 text-md text-gray-600 whitespace-pre-wrap placeholder:italic border-none p-0 ring-0 focus:ring-0'
+                placeholder='Cliquer ici pour ajouter une légende'
+                defaultValue={latestCaption || ''}
+                onChange={onCaptionChange}
+              />
+              <div className='flex-0 h-6 w-8'>
+                <StatusIndicator status={autosaveStatus} />
+              </div>
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   )
+}
+
+const MessageByStatus: Record<MediaStatus, string> = {
+  0: 'En cours de traitement',
+  1: 'En cours de traitement',
+  2: 'En cours de traitement',
+  3: 'En cours de transcodage',
+  4: 'Terminé',
+  5: 'Erreur',
+  6: 'Upload échoué',
+  404: 'Média non-trouvé',
 }
