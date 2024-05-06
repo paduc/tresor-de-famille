d696e9fec470fdec4de5fa765b74b71c28c7fab9
Pierre Duchateau <pad@dabla.com>
2024-05-06 09:28:24 +0200

    Navigate automaticaly to non-empty photo list

--- src/pages/photoList/photoList.route.ts
+++ src/pages/photoList/photoList.route.ts
@@ -2,6 +2,8 @@ import { z } from 'zod'
 import { requireAuth } from '../../dependencies/authn.js'
 import { zIsFamilyId } from '../../domain/FamilyId.js'
 import { responseAsHtml } from '../../libs/ssr/responseAsHtml.js'
+import { asFamilyId } from '../../libs/typeguards.js'
+import { getUserFamilies } from '../_getUserFamilies.js'
 import { pageRouter } from '../pageRouter.js'
 import { PhotoListPage } from './PhotoListPage.js'
 import { PhotoListPageUrlWithFamily } from './PhotoListPageUrl.js'
@@ -13,6 +15,23 @@ pageRouter.route(PhotoListPageUrlWithFamily()).get(requireAuth(), async (request
 
     const userId = request.session.user!.id
     const props = await getPhotoListPageProps({ userId, familyId })
+
+    if (!familyId && props.photos.length === 0) {
+      // Look for family with photos
+      const userFamilies = await getUserFamilies(userId)
+      for (const family of userFamilies) {
+        if (family.familyId !== asFamilyId(userId)) {
+          const photoListProps = await getPhotoListPageProps({
+            userId,
+            familyId: family.familyId,
+          })
+
+          if (photoListProps.photos.length > 0) {
+            return response.redirect(PhotoListPageUrlWithFamily(family.familyId))
+          }
+        }
+      }
+    }
 
     responseAsHtml(request, response, PhotoListPage(props))
   } catch (error) {
